9fb8b1eac143c7601f4248b68f8c0a5bfd835b61
Katy Davoudi <kdavoudi@ksu.edu>
2020-08-28 09:45:45 -0500

    Draft3!

--- Data/Entrée classes/BriarheartBurger.cs
+++ Data/Entrée classes/BriarheartBurger.cs
@@ -6,9 +6,69 @@ namespace BleakwindBuffet.Data.Entrees
 {
     public class BriarheartBurger
     {
-        public double Price
+        //gets price of the burger
+        public double Price =>  6.32;
+
+        public uint Calories => 732;
+
+        private bool ketchup = true; 
+        public bool Ketchup
         {
-            get{ return 6.32; }
+            get
+            {
+                return ketchup;
+            }
+            set
+            {
+                
+                ketchup = value;
+            }
         }
+
+        private bool bun = true;
+        public bool Bun
+        {
+            get
+            {
+                return bun;
+            }
+            set
+            {
+                bun = value;
+            }
+        }
+
+        private bool musturd = true;
+        public bool Musturd
+        {
+            get
+            {
+                return musturd;
+            }
+            set
+            {
+                musturd = value;
+            }
+        }
+
+        
+        public List<string> SpecialInstructions
+        {
+            get
+            {
+                List<string> instruction = new List<string>();
+                if (!Ketchup) instruction.Add("Hold ketchup");
+                if (!Bun) instruction.Add("Hold bun");
+                return instruction;
+            }
+        }
+
+        public override string ToString()
+        {
+            return "Briarheart Burger";
+        }
+
+
+
     }
 }
