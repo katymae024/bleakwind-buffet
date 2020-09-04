b4993f95bf5f7858611d973737d3d5b6ce90bdf1
kdavoudi <kdavoudi@CIS-RDS3>
2020-09-04 15:05:08 -0500

    draft 7

--- DataTests/UnitTests/EntreeTests/DoubleDraugrTests.cs
+++ DataTests/UnitTests/EntreeTests/DoubleDraugrTests.cs
@@ -6,6 +6,7 @@
 using Xunit;
 
 using BleakwindBuffet.Data;
+using BleakwindBuffet.Data.Entrees;
 
 namespace BleakwindBuffet.DataTests.UnitTests.EntreeTests
 {
@@ -14,66 +15,107 @@ namespace BleakwindBuffet.DataTests.UnitTests.EntreeTests
         [Fact]
         public void ShouldIncludeBunByDefault()
         {
+            DoubleDraugr dd = new DoubleDraugr();
+            Assert.True(dd.Bun);
         }
 
         [Fact]
         public void ShouldIncludeKetchupByDefault()
         {
+            DoubleDraugr dd = new DoubleDraugr();
+            Assert.True(dd.Ketchup);
         }
 
         [Fact]
         public void ShouldIncludeMustardByDefault()
         {
+            DoubleDraugr dd = new DoubleDraugr();
+            Assert.True(dd.Mustard);
         }
 
         [Fact]
         public void ShouldIncludePickleByDefault()
         {
+            DoubleDraugr dd = new DoubleDraugr();
+            Assert.True(dd.Pickle);
         }
 
         [Fact]
         public void ShouldIncludeCheeseByDefault()
         {
+            DoubleDraugr dd = new DoubleDraugr();
+            Assert.True(dd.Cheese);
         }
 
         [Fact]
         public void ShouldIncludeTomatoByDefault()
         {
+            DoubleDraugr dd = new DoubleDraugr();
+            Assert.True(dd.Tomato);
         }
 
         [Fact]
         public void ShouldIncludeLettuceByDefault()
         {
+            DoubleDraugr dd = new DoubleDraugr();
+            Assert.True(dd.Lettuce);
         }
 
         [Fact]
         public void ShouldIncludeMayoByDefault()
         {
+            DoubleDraugr dd = new DoubleDraugr();
+            Assert.True(dd.Mayo);
         }
 
         [Fact]
         public void ShouldBeAbleToSetBun()
         {
+            DoubleDraugr dd = new DoubleDraugr();
+            dd.Bun = true;
+            Assert.True(dd.Bun);
+            dd.Bun = false;
+            Assert.False(dd.Bun);
         }
 
         [Fact]
         public void ShouldBeAbleToSetKetchup()
         {
+            DoubleDraugr dd = new DoubleDraugr();
+            dd.Ketchup = true;
+            Assert.True(dd.Ketchup);
+            dd.Ketchup = false;
+            Assert.False(dd.Ketchup);
         }
 
         [Fact]
         public void ShouldBeAbleToSetMustard()
         {
+            DoubleDraugr dd = new DoubleDraugr();
+            dd.Mustard = true;
+            Assert.True(dd.Mustard);
+            dd.Mustard = false;
+            Assert.False(dd.Mustard);
         }
 
         [Fact]
         public void ShouldBeAbleToSetPickle()
         {
+            DoubleDraugr dd = new DoubleDraugr();
+            dd.Pickle = true;
+            Assert.True(dd.Pickle);
+            dd.Pickle = false;
+            Assert.False(dd.Pickle);
         }
 
         [Fact]
         public void ShouldBeAbleToSetCheese()
         {
+            DoubleDraugr dd = new DoubleDraugr();
+            dd.Cheese = true;
+            Assert.True(dd.Cheese);
+            dd.Cheese = false;
+            Assert.False(dd.Cheese);
         }
 
         [Fact]
