3b822a09c90fef31d1a069a5d4df9eda9c7ad026
kdavoudi <kdavoudi@CIS-RDS3>
2020-09-10 15:53:24 -0500

    Draft4

---
+++ Data/Menu.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using BleakwindBuffet.Data.Interfaces;
+using BleakwindBuffet.Data.Enums;
+using BleakwindBuffet.Data.Drinks;
+using BleakwindBuffet.Data.Entrée_classes;
+
+
+
+namespace BleakwindBuffet.Data
+{
+    public static class Menu
+    {
+        public static IEnumerable<IOrderItem> Entrees()
+        {
+            List<IOrderItem> entreeList = new List<IOrderItem>();
+
+            return entreeList;
+        }
+
+        public static IEnumerable<IOrderItem> Drinks()
+        {
+            List<IOrderItem> drinkList = new List<IOrderItem>();
+            foreach(Size s in Enum.GetValues(typeof(Size)))
+            {
+                AretinoAppleJuice a = new AretinoAppleJuice();
+                a.Size = s;
+                drinkList.Add(a);
+
+                SailorSoda ss = new SailorSoda();
+                ss.Size = s;
+                foreach(SodaFlavor f in Enum.GetValues(typeof(SodaFlavor)))
+                {
+                    ss.Flavor = f;
+                    drinkList.Add(ss);
+                }
+            }
+            
+            return drinkList;
+
+
+        }
+
+    }
+}
