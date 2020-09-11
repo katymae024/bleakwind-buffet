15eb1e621a3f6c1a4d2fc5403ca8f3ec6ba541ce
kdavoudi <kdavoudi@CIS-RDS3>
2020-09-11 14:29:27 -0500

    almost final

--- Data/Menu.cs
+++ Data/Menu.cs
@@ -5,6 +5,9 @@ using BleakwindBuffet.Data.Interfaces;
 using BleakwindBuffet.Data.Enums;
 using BleakwindBuffet.Data.Drinks;
 using BleakwindBuffet.Data.Entrée_classes;
+using BleakwindBuffet.Data.Entrees;
+using BleakwindBuffet.Data.Sides;
+
 
 
 
@@ -16,13 +19,35 @@ namespace BleakwindBuffet.Data
         {
             List<IOrderItem> entreeList = new List<IOrderItem>();
 
+            BriarheartBurger b = new BriarheartBurger();
+            entreeList.Add(b);
+
+            DoubleDraugr d = new DoubleDraugr();
+            entreeList.Add(d);
+
+            GardenOrcOmelette g = new GardenOrcOmelette();
+            entreeList.Add(g);
+
+            PhillyPoacher p = new PhillyPoacher();
+            entreeList.Add(p);
+
+            SmokehouseSkeleton s = new SmokehouseSkeleton();
+            entreeList.Add(s);
+
+            ThalmorTriple t = new ThalmorTriple();
+            entreeList.Add(t);
+
+            ThugsTBone tt = new ThugsTBone();
+            entreeList.Add(tt);
+
+
             return entreeList;
         }
 
         public static IEnumerable<IOrderItem> Drinks()
         {
             List<IOrderItem> drinkList = new List<IOrderItem>();
-            foreach(Size s in Enum.GetValues(typeof(Size)))
+            foreach (Size s in Enum.GetValues(typeof(Size)))
             {
                 AretinoAppleJuice a = new AretinoAppleJuice();
                 a.Size = s;
@@ -30,17 +55,56 @@ namespace BleakwindBuffet.Data
 
                 SailorSoda ss = new SailorSoda();
                 ss.Size = s;
-                foreach(SodaFlavor f in Enum.GetValues(typeof(SodaFlavor)))
+                foreach (SodaFlavor f in Enum.GetValues(typeof(SodaFlavor)))
                 {
                     ss.Flavor = f;
                     drinkList.Add(ss);
                 }
+
+                CandlehearthCoffee c = new CandlehearthCoffee();
+                c.Size = s;
+                drinkList.Add(c);
+
+                MarkarthMilk m = new MarkarthMilk();
+                m.Size = s;
+                drinkList.Add(m);
+
+                WarriorWater w = new WarriorWater();
+                w.Size = s;
+                drinkList.Add(w);
+
             }
-            
+
             return drinkList;
 
 
         }
 
+        public static IEnumerable<IOrderItem> Sides()
+        {
+            List<IOrderItem> sideList = new List<IOrderItem>();
+            foreach (Size f in Enum.GetValues(typeof(Size)))
+            {
+                DragonbornWaffleFries d = new DragonbornWaffleFries();
+                d.Size = f;
+                sideList.Add(d);
+
+                FriedMiraak fm = new FriedMiraak();
+                fm.Size = f;
+                sideList.Add(fm);
+
+                MadOtarGrits m = new MadOtarGrits();
+                m.Size = f;
+                sideList.Add(m);
+
+                VokunSalad v = new VokunSalad();
+                v.Size = f;
+                sideList.Add(v);
+
+
+
+            }
+            return sideList;
+        }
     }
 }
