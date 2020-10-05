34f577e044d678930ce1f9e7b49da750c8edc424
kdavoudi <kdavoudi@CIS-RDS2>
2020-10-05 16:51:51 -0500

    draft1

---
+++ Data/ImplementFunctionalityCombo.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel;
+using BleakwindBuffet.Data.Interfaces;
+using BleakwindBuffet.Data.Entrees;
+using BleakwindBuffet.Data.Entrée_classes;
+using BleakwindBuffet.Data.Drinks;
+using BleakwindBuffet.Data.Side_Classes;
+
+namespace BleakwindBuffet.Data
+{
+    public class ImplementFunctionalityCombo : IOrderItem, INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private Entree entree;
+        private Drink drink;
+        private Side side;
+
+
+        public double Price => throw new NotImplementedException();
+
+        public uint Calories => throw new NotImplementedException();
+
+        public List<string> SpecialInstructions
+        {
+            get
+            {
+                throw new NotImplementedException();
+
+                List<string> list = new List<string>();
+                list.Add(entree.ToString());
+                list.AddRange(entree.SpecialInstructions);
+                list.Add(drink.ToString());
+                list.AddRange(drink.SpecialInstructions);
+                list.Add(side.ToString());
+                list.AddRange(side.SpecialInstructions);
+                return list;
+            }
+            
+        }
+
+
+        
+
+
+
+    }
+}
