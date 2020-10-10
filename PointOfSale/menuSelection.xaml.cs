625045886473074151fd2d6302675c0675de867f
kdavoudi <kdavoudi@CIS-RDS2>
2020-10-10 13:32:17 -0500

    draft10

--- PointOfSale/menuSelection.xaml.cs
+++ PointOfSale/menuSelection.xaml.cs
@@ -76,10 +76,17 @@ namespace PointOfSale
         /// <param name="e"> routed evet args called e</param>
         private void TT_Click(object sender, RoutedEventArgs e)
         {
-            var orderControl = this.FindRoot<OrderControl>();
-            var screen = new ThalmorTriple();
-            screen.DataContext = new BleakwindBuffet.Data.Entrees.ThalmorTriple();
-            orderControl.SwapScreen(screen);
+            if (DataContext is BleakwindBuffet.Data.OrderClass order)
+            {
+
+                var tt = new BleakwindBuffet.Data.Entrees.ThalmorTriple();
+                var orderControl = this.FindRoot<OrderControl>();
+                var screen = new ThalmorTriple();
+                screen.DataContext = tt;
+                orderControl.SwapScreen(screen);
+                order.Add(tt);
+
+            }
         }
         /// <summary>
         /// Swaps screen from menu
@@ -88,10 +95,15 @@ namespace PointOfSale
         /// <param name="e"> routed evet args called e</param>
         private void SS_Click(object sender, RoutedEventArgs e)
         {
-            var orderControl = this.FindRoot<OrderControl>();
-            var screen = new SmokehouseSkeleton();
-            screen.DataContext = new BleakwindBuffet.Data.Entrees.SmokehouseSkeleton();
-            orderControl.SwapScreen(screen);
+            if (DataContext is BleakwindBuffet.Data.OrderClass order)
+            {
+                var ss = new BleakwindBuffet.Data.Entrees.SmokehouseSkeleton();
+                var orderControl = this.FindRoot<OrderControl>();
+                var screen = new SmokehouseSkeleton();
+                screen.DataContext = ss;
+                orderControl.SwapScreen(screen);
+                order.Add(ss);
+            }
         }
         /// <summary>
         /// Swaps screen from menu
@@ -100,10 +112,16 @@ namespace PointOfSale
         /// <param name="e"> routed evet args called e</param>
         private void GO_Click(object sender, RoutedEventArgs e)
         {
-            var orderControl = this.FindRoot<OrderControl>();
-            var screen = new GardenOrcOmelette();
-            screen.DataContext = new BleakwindBuffet.Data.Entrees.GardenOrcOmelette();
-            orderControl.SwapScreen(screen);
+            if (DataContext is BleakwindBuffet.Data.OrderClass order)
+            {
+
+                var go = new BleakwindBuffet.Data.Entrees.GardenOrcOmelette();
+                var orderControl = this.FindRoot<OrderControl>();
+                var screen = new GardenOrcOmelette();
+                screen.DataContext = go;
+                orderControl.SwapScreen(screen);
+                order.Add(go);
+            }
         }
         /// <summary>
         /// doesn't do anything since thugs t-bone doesn't have special instructions
@@ -112,7 +130,11 @@ namespace PointOfSale
         /// <param name="e"> routed evet args called e</param>
         private void TTB_Click(object sender, RoutedEventArgs e)
         {
-
+            if (DataContext is BleakwindBuffet.Data.OrderClass order)
+            {
+                var ttb = new BleakwindBuffet.Data.Entrees.ThugsTBone();
+                order.Add(ttb);
+            }
         }
         /// <summary>
         /// Swaps screen from menu
@@ -121,10 +143,15 @@ namespace PointOfSale
         /// <param name="e"> routed evet args called e</param>
         private void SSoda_Click(object sender, RoutedEventArgs e)
         {
-            var orderControl = this.FindRoot<OrderControl>();
-            var screen = new SailorSoda();
-            screen.DataContext = new BleakwindBuffet.Data.Drinks.SailorSoda();
-            orderControl.SwapScreen(screen);
+            if (DataContext is BleakwindBuffet.Data.OrderClass order)
+            {
+                var ssoda = new BleakwindBuffet.Data.Drinks.SailorSoda();
+                var orderControl = this.FindRoot<OrderControl>();
+                var screen = new SailorSoda();
+                screen.DataContext = ssoda;
+                orderControl.SwapScreen(screen);
+                order.Add(ssoda);
+            }
         }
         /// <summary>
         /// Swaps screen from menu
@@ -133,10 +160,15 @@ namespace PointOfSale
         /// <param name="e"> routed evet args called e</param>
         private void MM_Click(object sender, RoutedEventArgs e)
         {
-            var orderControl = this.FindRoot<OrderControl>();
-            var screen = new MarkathMilk();
-            screen.DataContext = new BleakwindBuffet.Data.Drinks.MarkarthMilk();
-            orderControl.SwapScreen(screen);
+            if (DataContext is BleakwindBuffet.Data.OrderClass order)
+            {
+                var mm = new BleakwindBuffet.Data.Drinks.MarkarthMilk();
+                var orderControl = this.FindRoot<OrderControl>();
+                var screen = new MarkathMilk();
+                screen.DataContext = mm;
+                orderControl.SwapScreen(screen);
+                order.Add(mm);
+            }
         }
         /// <summary>
         /// Swaps screen from menu
@@ -145,11 +177,15 @@ namespace PointOfSale
         /// <param name="e"> routed evet args called e</param>
         private void AA_Click(object sender, RoutedEventArgs e)
         {
-
-            var orderControl = this.FindRoot<OrderControl>();
-            var screen = new AretinoAppleJuice();
-            screen.DataContext = new BleakwindBuffet.Data.Drinks.AretinoAppleJuice();
-            orderControl.SwapScreen(screen);
+            if (DataContext is BleakwindBuffet.Data.OrderClass order)
+            {
+                var aa = new BleakwindBuffet.Data.Drinks.AretinoAppleJuice();
+                var orderControl = this.FindRoot<OrderControl>();
+                var screen = new AretinoAppleJuice();
+                screen.DataContext = aa;
+                orderControl.SwapScreen(screen);
+                order.Add(aa);
+            }
             
         }
         /// <summary>
@@ -159,10 +195,16 @@ namespace PointOfSale
         /// <param name="e"> routed evet args called e</param>
         private void CC_Click(object sender, RoutedEventArgs e)
         {
-            var orderControl = this.FindRoot<OrderControl>();
-            var screen = new CandlehearthCoffee();
-            screen.DataContext = new BleakwindBuffet.Data.Drinks.CandlehearthCoffee();
-            orderControl.SwapScreen(screen);
+            if (DataContext is BleakwindBuffet.Data.OrderClass order)
+            {
+                var cc = new BleakwindBuffet.Data.Drinks.CandlehearthCoffee();
+                var orderControl = this.FindRoot<OrderControl>();
+                var screen = new CandlehearthCoffee();
+                screen.DataContext = cc;
+                orderControl.SwapScreen(screen);
+                order.Add(cc);
+
+            }
         }
         /// <summary>
         /// Swaps screen from menu
@@ -171,10 +213,15 @@ namespace PointOfSale
         /// <param name="e"> routed evet args called e</param>
         private void WW_Click(object sender, RoutedEventArgs e)
         {
-            var orderControl = this.FindRoot<OrderControl>();
-            var screen = new WarriorWater();
-            screen.DataContext = new BleakwindBuffet.Data.Drinks.WarriorWater();
-            orderControl.SwapScreen(screen);
+            if (DataContext is BleakwindBuffet.Data.OrderClass order)
+            {
+                var ww = new BleakwindBuffet.Data.Drinks.WarriorWater();
+                var orderControl = this.FindRoot<OrderControl>();
+                var screen = new WarriorWater();
+                screen.DataContext = ww;
+                orderControl.SwapScreen(screen);
+                order.Add(ww);
+            }
         }
         /// <summary>
         /// Swaps screen from menu
@@ -183,10 +230,15 @@ namespace PointOfSale
         /// <param name="e"> routed evet args called e</param>
         private void VS_Click(object sender, RoutedEventArgs e)
         {
-            var orderControl = this.FindRoot<OrderControl>();
-            var screen = new VokunSalad();
-            screen.DataContext = new BleakwindBuffet.Data.Sides.VokunSalad();
-            orderControl.SwapScreen(screen);
+            if (DataContext is BleakwindBuffet.Data.OrderClass order)
+            {
+                var vs = new BleakwindBuffet.Data.Sides.VokunSalad();
+                var orderControl = this.FindRoot<OrderControl>();
+                var screen = new VokunSalad();
+                screen.DataContext = vs;
+                orderControl.SwapScreen(screen);
+                order.Add(vs);
+            }
         }
         /// <summary>
         /// Swaps screen from menu
@@ -195,10 +247,16 @@ namespace PointOfSale
         /// <param name="e"> routed evet args called e</param>
         private void FM_Click(object sender, RoutedEventArgs e)
         {
-            var orderControl = this.FindRoot<OrderControl>();
-            var screen = new FriedMiraak();
-            screen.DataContext = new BleakwindBuffet.Data.Sides.FriedMiraak();
-            orderControl.SwapScreen(screen);
+            if (DataContext is BleakwindBuffet.Data.OrderClass order)
+            {
+                var fm = new BleakwindBuffet.Data.Sides.FriedMiraak();
+                var orderControl = this.FindRoot<OrderControl>();
+                var screen = new FriedMiraak();
+                screen.DataContext = fm;
+                orderControl.SwapScreen(screen);
+                order.Add(fm);
+            }
+
         }
         /// <summary>
         /// Swaps screen from menu
@@ -207,10 +265,15 @@ namespace PointOfSale
         /// <param name="e"> routed evet args called e</param>
         private void MOG_Click(object sender, RoutedEventArgs e)
         {
-            var orderControl = this.FindRoot<OrderControl>();
-            var screen = new MadOtarGrits();
-            screen.DataContext = new BleakwindBuffet.Data.Sides.MadOtarGrits();
-            orderControl.SwapScreen(screen);
+            if (DataContext is BleakwindBuffet.Data.OrderClass order)
+            {
+                var mog = new BleakwindBuffet.Data.Sides.MadOtarGrits();
+                var orderControl = this.FindRoot<OrderControl>();
+                var screen = new MadOtarGrits();
+                screen.DataContext = mog;
+                orderControl.SwapScreen(screen);
+                order.Add(mog);
+            }
         }
         /// <summary>
         /// Swaps screen from menu
@@ -219,10 +282,15 @@ namespace PointOfSale
         /// <param name="e"> routed evet args called e</param>
         private void DWF_Click(object sender, RoutedEventArgs e)
         {
-            var orderControl = this.FindRoot<OrderControl>();
-            var screen = new DragonbornWaffleFries();
-            screen.DataContext = new BleakwindBuffet.Data.Sides.DragonbornWaffleFries();
-            orderControl.SwapScreen(screen);
+            if (DataContext is BleakwindBuffet.Data.OrderClass order)
+            {
+                var dwf = new BleakwindBuffet.Data.Sides.DragonbornWaffleFries();
+                var orderControl = this.FindRoot<OrderControl>();
+                var screen = new DragonbornWaffleFries();
+                screen.DataContext = dwf;
+                orderControl.SwapScreen(screen);
+                order.Add(dwf);
+            }
         }
         /// <summary>
         /// Swaps screen from menu
@@ -231,10 +299,15 @@ namespace PointOfSale
         /// <param name="e"> routed evet args called e</param>
         private void PP_Click(object sender, RoutedEventArgs e)
         {
-            var orderControl = this.FindRoot<OrderControl>();
-            var screen = new PhillyPoacher();
-            screen.DataContext = new BleakwindBuffet.Data.Entrees.PhillyPoacher();
-            orderControl.SwapScreen(screen);
+            if (DataContext is BleakwindBuffet.Data.OrderClass order)
+            {
+                var pp = new BleakwindBuffet.Data.Entrees.PhillyPoacher();
+                var orderControl = this.FindRoot<OrderControl>();
+                var screen = new PhillyPoacher();
+                screen.DataContext = pp;
+                orderControl.SwapScreen(screen);
+                order.Add(pp);
+            }
         }
     }
 }
