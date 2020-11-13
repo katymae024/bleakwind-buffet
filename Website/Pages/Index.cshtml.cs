77db1742797db3816d44782ed7396e39ba1d36f2
kdavoudi <kdavoudi@CIS-RDS2>
2020-11-13 13:58:22 -0600

    draft6

--- Website/Pages/Index.cshtml.cs
+++ Website/Pages/Index.cshtml.cs
@@ -7,11 +7,14 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BleakwindBuffet.Data.Drink_Classes;
+using BleakwindBuffet.Data.Entrée_classes;
 using BleakwindBuffet.Data.Interfaces;
+using BleakwindBuffet.Data.Side_Classes;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
-using System.Linq;
+
 
 namespace Website.Pages
 {
@@ -34,8 +37,12 @@ namespace Website.Pages
         public double? PriceMax { get; set; }
         [BindProperty]
         public string Description { get; }
-      
 
+       [BindProperty] //should it have this?
+       public int Calories { get; }
+
+       [BindProperty] //should it have this?
+        public int Price { get; }
 
         public IEnumerable<IOrderItem> MenuItems { get; set; }
 
@@ -43,7 +50,7 @@ namespace Website.Pages
 
         public string SearchTerms { get; set; }
 
-        public void OnGet(string SearchTerms,string Description, string[] Types, double? PriceMin, double? PriceMax, int? CaloriesMin, int? CaloriesMax)
+        public void OnGet(string SearchTerms, string[] Types, double? PriceMin, double? PriceMax, int? CaloriesMin, int? CaloriesMax)
         {
           
             //MenuItems = MenuDatabase.Search(SearchTerms);
@@ -55,11 +62,57 @@ namespace Website.Pages
             //MenuItems = MenuDatabase.FilterByCalories(MenuItems, CaloriesMin, CaloriesMax );
             //MenuItems = MenuDatabase.FilterByPrice(MenuItems, PriceMin, PriceMax);
             MenuItems = MenuDatabase.All;
+            SearchTerms = Request.Query["SearchTerms"];
+            Types = Request.Query["Types"];
             // Search movie titles for the SearchTerms
             if (SearchTerms != null)
             {
                 MenuItems = MenuItems.Where(item => item.ToString() != null && item.ToString().Contains(SearchTerms, StringComparison.InvariantCultureIgnoreCase));
             }
+
+            if (Types != null && Types.Length != 0)
+            {
+                MenuItems = MenuItems.Where(item => item is Entree && Types.Contains("Entrees") || item is Side && Types.Contains("Sides") || item is Drink && Types.Contains("Drinks"));
+            }
+
+            if (CaloriesMin == null && CaloriesMax == null)
+            {
+
+            }
+            else if (CaloriesMin == null)
+            {
+                
+                MenuItems = MenuItems.Where(item => item.Calories != null && item.Calories <= CaloriesMax);
+
+            }
+            else if (CaloriesMax == null)
+            {
+                MenuItems = MenuItems.Where(item => item.Calories != null && item.Calories >= CaloriesMin);
+            }
+            else
+            {
+                MenuItems = MenuItems.Where(item => item.Calories != null && item.Calories >= CaloriesMin && item.Calories <= CaloriesMax);
+            }
+
+            if (PriceMin == null && PriceMax == null)
+            {
+
+            }
+            else if (PriceMin == null)
+            {
+                
+                MenuItems = MenuItems.Where(item => item.Price != null && item.Price <= PriceMax);
+
+            }
+            else if (PriceMax == null)
+            {
+                MenuItems = MenuItems.Where(item => item.Price != null && item.Price >= PriceMin);
+            }
+            else
+            {
+                MenuItems = MenuItems.Where(item => item.Price != null && item.Price >= PriceMin && item.Price <= PriceMax);
+            }
+
         }
 
         public object PersistCheckbox(string type)
